f7964fb1a66c04c061645d210e97e8886baa087b
LAULAUtonton <lestevezplasencia@gmail.com>
2026-02-06 09:04:04 +0000

    Update index.js

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,14 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import { Routes, Route, Link } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+function Home() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>GLOBALISSUES</h1>
+      <p>This is the homepage.</p>
+      <Link to="/instructions">Go to instructions</Link>
+    </div>
+  );
+}
 
-root.render(
-  <React.StrictMode>
-    <BrowserRouter basename="/GLOBALISSUES">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+function Instructions() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h2>Here are your Instructions</h2>
+      <Link to="/">Back home</Link>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/instructions" element={<Instructions />} />
+      <Route path="*" element={<Home />} />
+    </Routes>
+  );
+}
