c7de07d42ae5cbee6f438a8b66e4b37a54d14d32
LAULAUtonton <lestevezplasencia@gmail.com>
2026-02-06 04:22:29 +0000

    Update App.js

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,1 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "40px", fontSize: "24px" }}>
+      <h1>Global Issues</h1>
+      <p>The app is working 🎉</p>
+    </div>
+  );
+}
 
+export default App;
